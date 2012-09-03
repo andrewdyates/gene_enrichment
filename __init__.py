cd69343bf8561f5dffcfab181c6895b9f86c6958
Andrew Yates <andrewyates.name@gmail.com>
2012-09-03 18:45:40 -0400

    enrichment curve plotting

--- __init__.py
+++ __init__.py
@@ -2,7 +2,46 @@
 import random
 from enriched import *
 
+import matplotlib 
+matplotlib.use('agg') # required for use on OSC servers
+import matplotlib.pyplot as plt
+import numpy as np
+
+COLORS = ['r', 'b', 'g', 'y', 'c', 'm', 'k', '0.50']
+PATTERNS = ['-', ':', '-.']
+STYLES = []
+for pattern in PATTERNS:
+  for color in COLORS:
+    STYLES.append({'color': color, 'linestyle': pattern})
+
+
 def random_indices(n, size):
   return random.sample(xrange(int(n*(n-1)/2)), size)
 
 
+def make_enrichment_curve_figure(Ranks, title=None, plotpath=None):
+  if title is None:
+    title = "Gene Enrichment"
+  plt.clf(); plt.cla()
+  plt.legend()
+  plt.xlabel('Rank')
+  plt.ylabel('# Matched')
+  plt.title(title)
+  plot_enrichments(Ranks)
+  if plotpath is None:
+    plt.show()
+  else:
+    plt.savefig(plotpath, dpi=600)
+
+
+def plot_enrichments(Ranks):
+  """PyPlot a dictionary of ranks."""
+  style_i = 0
+  for name, ranks in Ranks.items():
+    x = ranks
+    y = np.arange(len(x))+1
+    plt.plot(x, y, label=name, **STYLES[style_i]);
+    style_i += 1
+  
+
+                     
